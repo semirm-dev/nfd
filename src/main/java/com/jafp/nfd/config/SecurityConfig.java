d210dbbcc87deb8d870e947753b17f4b5d6ace07
Semir Mahovkic <semirm.dev@gmail.com>
2020-10-21 10:57:41 +0200

    nfd

--- src/main/java/com/jafp/nfd/config/SecurityConfig.java
+++ src/main/java/com/jafp/nfd/config/SecurityConfig.java
@@ -1,19 +1,21 @@
 package com.jafp.nfd.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest()
-                .permitAll()
-                .and().csrf().disable();
+        http
+                .authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/demo").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin();
     }
 }
